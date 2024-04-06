de1ac6739977752d16c3d31ffd8dab95a6801486
Franco Ramos <francoramos1511@gmail.com>
2024-04-06 18:34:57 -0300

    [GREEN] CreatingEmptyPersonShouldReturnEmpty

---
+++ Proyecto/Obligatorio1/Model/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Model;
+
+public class Person
+{
+    private string _name { get; set; }
+    private string _surname { get; set; }
+    private string _email { get; set; }
+    private string _password { get; set; }
+
+    public Person()
+    {
+        
+    }
+}
