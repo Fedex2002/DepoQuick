7c28b07b0d7fb05f394e6996d28185ac6db74cfc
Franco Ramos <francoramos1511@gmail.com>
2024-04-06 18:32:00 -0300

    [RED] CreatingEmptyPersonShouldReturnEmpty

---
+++ Proyecto/Obligatorio1/ModelTests/PersonTests.cs
@@ -0,0 +1,20 @@
+namespace ModelTests;
+
+[TestClass]
+public class PersonTests
+{
+    private Person _myperson;
+    
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _myperson = new Person();
+    }
+    
+    [TestMethod]
+    public void CreatingEmptyPersonShouldReturnEmpty()
+    {
+        Assert.IsNotNull(_myperson);
+    }
+    
+}
