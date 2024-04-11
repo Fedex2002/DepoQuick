9fd287a072ba20892f2bbf5dd947ddc0ebfbcdec
Franco Ramos <francoramos1511@gmail.com>
2024-04-11 17:35:41 -0300

    [RED] CreatingAStorageUnitWithValues_ShouldReturnValues

--- Proyecto/Obligatorio1/ModelTests/StorageUnitTests.cs
+++ Proyecto/Obligatorio1/ModelTests/StorageUnitTests.cs
@@ -1,3 +1,4 @@
+using System.Transactions;
 using Model;
 namespace ModelTests;
 
@@ -16,5 +17,18 @@ public class StorageUnitTests
     public void CreatingEmptyStorageUnitShouldReturnEmpty()
     {
         Assert.IsNotNull(_mystorageunit);
+    }
+    
+    [TestMethod]
+    public void CreatingAStorageUnitWithValues_ShouldReturnValues()
+    {
+        AreaType area = AreaType.A;
+        SizeType size = SizeType.Small;
+        Promotion p = new Promotion();
+        StorageUnit storageUnit = new StorageUnit(area, size, true, p);
+        Assert.AreEqual(AreaType.A, storageUnit.GetArea());
+        Assert.AreEqual(SizeType.Small, storageUnit.GetSize());
+        Assert.AreEqual(true, storageUnit.GetClimatization());
+        Assert.AreEqual(p, storageUnit.GetPromotion());
     }
 }
