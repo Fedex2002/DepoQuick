6ce5e2bbc059b00a5defed223846ed8b80fe6cb6
Franco Ramos <francoramos1511@gmail.com>
2024-04-12 18:48:16 -0300

    [GREEN] CreatingBookingWithValidations_ShouldReturnValidValues

--- Proyecto/Obligatorio1/Model/Booking.cs
+++ Proyecto/Obligatorio1/Model/Booking.cs
@@ -9,4 +9,26 @@ public class Booking
     public Booking()
     {
     }
+    
+    public Booking(bool approved, DateTime dateStart, DateTime dateEnd)
+    {
+        this.approved = approved;
+        this.dateStart = dateStart;
+        this.dateEnd = dateEnd;
+    }
+    
+    public bool GetApproved()
+    {
+        return approved;
+    }
+    
+    public DateTime GetDateStart()
+    {
+        return dateStart;
+    }
+    
+    public DateTime GetDateEnd()
+    {
+        return dateEnd;
+    }
 }
