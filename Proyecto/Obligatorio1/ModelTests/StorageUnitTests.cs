112a84198374b0381012bf0df2281cdbffe21bfd
Federico Gutierrez <fedeguti521@gmail.com>
2024-04-13 17:11:26 -0300

    [REFACTOR] CalculatingStorageUnitPricePerDayWithValidations_ShouldReturnPrice

--- Proyecto/Obligatorio1/ModelTests/StorageUnitTests.cs
+++ Proyecto/Obligatorio1/ModelTests/StorageUnitTests.cs
@@ -7,16 +7,22 @@ namespace ModelTests;
 public class StorageUnitTests
 {
     private StorageUnit _mystorageunit;
-
+    private List<Promotion> _promotions;
+    private Promotion _mypromotion;
+    
     [TestInitialize]
     public void TestInitialize()
     {
+        _promotions = new List<Promotion>();
+       _mypromotion = new Promotion("Descuento Invierno", 25, new DateTime(2024,7,15), new DateTime(2024,10,15));
+        _promotions.Add(_mypromotion);
         _mystorageunit = new StorageUnit();
     }
 
     [TestMethod]
     public void CreatingEmptyStorageUnitShouldReturnEmpty()
     {
+        
         Assert.IsNotNull(_mystorageunit);
     }
     
@@ -36,20 +42,15 @@ public class StorageUnitTests
     [TestMethod]
     public void CalculatingStorageUnitPricePerDayWithValidations_ShouldReturnPrice()
     {
-        List<Promotion> p = new List<Promotion>();
-        Promotion myPromotion = new Promotion("Descuento Invierno", 25, new DateTime(2024,7,15), new DateTime(2024,10,15));
-        p.Add(myPromotion);
+        Assert.AreEqual(0, _mystorageunit.CalculateStorageUnitPrice());
         
-        StorageUnit storageUnit = new StorageUnit();
-        Assert.AreEqual(0, storageUnit.CalculateStorageUnitPrice());
+        _mystorageunit= new StorageUnit(AreaType.A, SizeType.Small, true,_promotions );
+        Assert.AreEqual(52.5, _mystorageunit.CalculateStorageUnitPrice());
         
-        StorageUnit storageUnitSmall = new StorageUnit(AreaType.A, SizeType.Small, true, p);
-        Assert.AreEqual(52.5, storageUnitSmall.CalculateStorageUnitPrice());
+        _mystorageunit = new StorageUnit(AreaType.B, SizeType.Medium, false, null);
+        Assert.AreEqual(75, _mystorageunit.CalculateStorageUnitPrice());
         
-        StorageUnit storageUnitMedium = new StorageUnit(AreaType.B, SizeType.Medium, false, null);
-        Assert.AreEqual(75, storageUnitMedium.CalculateStorageUnitPrice());
-        
-        StorageUnit storageUnitLarge = new StorageUnit(AreaType.C, SizeType.Large, true, null);
-        Assert.AreEqual(120, storageUnitLarge.CalculateStorageUnitPrice());
+        _mystorageunit = new StorageUnit(AreaType.C, SizeType.Large, true, null);
+        Assert.AreEqual(120, _mystorageunit.CalculateStorageUnitPrice());
     }
 }
