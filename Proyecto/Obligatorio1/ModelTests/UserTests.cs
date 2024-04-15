dc5284c331cc56a8d9805367f12b04eff6e1f5b1
Franco Ramos <francoramos1511@gmail.com>
2024-04-15 16:49:04 -0300

    [RED] CreatingUserWithValidations_ShouldReturnValues

--- Proyecto/Obligatorio1/ModelTests/UserTests.cs
+++ Proyecto/Obligatorio1/ModelTests/UserTests.cs
@@ -18,4 +18,15 @@ public class UserTests
     {
         Assert.IsNotNull(_myuser);
     }
+
+    [TestMethod]
+    public void CreatingUserWithValidations_ShouldReturnValues()
+    {
+        _myuser = new User(string name, string surname, string email, string password, Booking _mybooking);
+        Assert.AreEqual(name, _myuser.GetName());
+        Assert.AreEqual(surname, _myuser.GetSurname());
+        Assert.AreEqual(email, _myuser.GetEmail());
+        Assert.AreEqual(password, _myuser.GetPassword());
+        Assert.AreEqual(_mybooking, _myuser.GetBooking());
+    }
 }
