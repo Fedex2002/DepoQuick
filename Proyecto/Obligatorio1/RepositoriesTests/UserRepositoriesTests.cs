fd05987d4f8653d48ccb7d32d034e96ed3f11f3b
Federico Gutierrez <fedeguti521@gmail.com>
2024-04-17 18:53:49 -0300

    [RED] WhenAddingNewUserShouldAddItToRepository

--- Proyecto/Obligatorio1/RepositoriesTests/UserRepositoriesTests.cs
+++ Proyecto/Obligatorio1/RepositoriesTests/UserRepositoriesTests.cs
@@ -1,10 +1,24 @@
+using Model;
+
 namespace RepositoriesTests;
 
 [TestClass]
-public class UnitTest1
+public class UserRepositoryTest
 {
-    [TestMethod]
-    public void TestMethod1()
+    private UserRepository _userepo;
+    private User _user;
+    [TestInitialize] 
+    public void TestInitialize()
     {
+        _userepo = new UserRepository();
+        _user = new User();
+    }
+
+    [TestMethod]
+    public void WhenAddingNewUserShouldAddItToRepository()
+    {
+        _userepo.AddUser(_user);
+        User userInRepo = _userepo.FindUser(_user);
+        Assert.AreEqual(_user.GetEmail(), userInRepo.GetEmail());
     }
 }
