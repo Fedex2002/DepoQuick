b171579b32b3358ef5a6082cb53297b731e934b0
Federico Gutierrez <fedeguti521@gmail.com>
2024-04-19 16:48:03 -0300

    [RED] WhenAPromotionExistsInRepositoryShouldFindIt

--- Proyecto/Obligatorio1/RepositoriesTests/PromotionsRepositoriesTests.cs
+++ Proyecto/Obligatorio1/RepositoriesTests/PromotionsRepositoriesTests.cs
@@ -25,4 +25,12 @@ public class PromotionsRepositoriesTests
         Promotion promotionInRepo = _promotionsRepositories.GetFromRepository(_promotion);
         Assert.AreEqual(_promotion.GetLabel(), promotionInRepo.GetLabel());
     }
+    
+    [TestMethod]
+    public void WhenAPromotionExistsInRepositoryShouldFindIt()
+    {
+        _promotion = new Promotion("Descuento Invierno", 25, new DateTime(2024, 7, 15), new DateTime(2024, 10, 15));
+        _promotionsRepositories.AddToRepository(_promotion);
+        Assert.IsTrue(_promotionsRepositories.ExistsInRepository(_promotion));
+    }
 }
