f749dd58cd8fc71ae4608fe6aabb06c8127fbf33
Federico Gutierrez <fedeguti521@gmail.com>
2024-04-21 22:14:31 -0300

    [RED] WhenAddingNewStorageUnitShouldAddItToRepository

---
+++ Proyecto/Obligatorio1/RepositoriesTests/StorageUnitRepositoriesTests.cs
@@ -0,0 +1,26 @@
+namespace RepositoriesTests;
+
+[TestClass]
+
+public class StorageUnitRepositoriesTests
+{
+    private StorageUnitRepositories _storageUnitRepositories;
+    private StorageUnit _storageUnit;
+    
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _storageUnitRepositories = new StorageUnitRepositories();
+        _storageUnit = new StorageUnit(0,AreaType.A, SizeType.Small, true,null );
+    }
+    
+    
+    [TestMethod]
+    public void WhenAddingNewStorageUnitShouldAddItToRepository()
+    {
+        _storageUnitRepositories.AddToRepository(_storageUnit);
+        StorageUnit storageUnitInRepo = _storageUnitRepositories.GetFromRepository(_storageUnit);
+        Assert.AreEqual(_storageUnit.GetId(), storageUnitInRepo.GetId());
+    }
+    
+}
