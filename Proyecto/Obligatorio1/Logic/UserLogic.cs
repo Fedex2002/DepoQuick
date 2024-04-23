ff60598f8f6a29d8ea67f6c19126a58e5affcd88
Federico Gutierrez <fedeguti521@gmail.com>
2024-04-22 21:40:05 -0300

    [REFACTOR] WhenEmailIsNotRegisteredThrowException

--- Proyecto/Obligatorio1/Logic/UserLogic.cs
+++ Proyecto/Obligatorio1/Logic/UserLogic.cs
@@ -5,10 +5,15 @@ namespace Logic;
 
 public class UserLogic
 {
-    private UserRepositories _userRepositories;
+    private UserRepositories _userRepositories= new UserRepositories();
 
     public bool CheckIfEmailIsRegistered(string email)
     {
-       throw new LogicExceptions("The email is not registered");
+        if (!_userRepositories.ExistsInRepository(email))
+        {
+            throw new LogicExceptions("The email is not registered");
+        }
+
+        return true;
     }
 }
