77cf318dd316528f59e5a29c1d2864c8430b86fb
Franco Ramos <francoramos1511@gmail.com>
2024-04-27 15:01:12 -0300

    [REFACTOR] WhenUserIsLoggedInSetItAsCurrentUser

--- Proyecto/Obligatorio1/Logic/SessionLogic.cs
+++ Proyecto/Obligatorio1/Logic/SessionLogic.cs
@@ -14,9 +14,7 @@ public class SessionLogic
     
     public void Login(string email,string password)
     {
-        User user = _userLogic.GetRepository().GetFromRepository(email);
         CurrentUser = _userLogic.Login(email,password); 
-       
     }
     
     public void Logout(User user)
