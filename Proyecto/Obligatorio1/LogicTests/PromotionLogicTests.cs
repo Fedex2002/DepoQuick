1e5d141deded4f2fffd5863da4997ecae8e59aef
Franco Ramos <francoramos1511@gmail.com>
2024-05-01 10:56:42 -0300

    [RED] WhenPromotionIsCreatedShouldBeAddedToRepository

--- Proyecto/Obligatorio1/LogicTests/PromotionLogicTests.cs
+++ Proyecto/Obligatorio1/LogicTests/PromotionLogicTests.cs
@@ -22,11 +22,23 @@ public class PromotionLogicTests
         _promotion = new Promotion("Winter discount", 25, new DateTime(2024, 7, 15), new DateTime(2024, 10, 15));
     }
     
-    [TestMethod] public void WhenModifyingPromotionShouldEliminateTheOldOneAndAddTheNewOne()
+    [TestMethod] 
+    public void WhenModifyingPromotionShouldEliminateTheOldOneAndAddTheNewOne()
     {
         _promotionRepo.AddToRepository(_promotion);
         _promotionDto= new PromotionDto("Summer discount", 30, new DateTime(2024, 7, 15), new DateTime(2024, 10, 15));
         _promotionLogic.ModifyPromotion(_promotionDto);
+        Assert.AreEqual(_promotionDto.Label, _promotionRepo.GetFromRepository(_promotionDto.Label).GetLabel());
+        Assert.AreEqual(_promotionDto.Discount, _promotionRepo.GetFromRepository(_promotionDto.Label).GetDiscount());
+        Assert.AreEqual(_promotionDto.DateStart, _promotionRepo.GetFromRepository(_promotionDto.Label).GetDateStart());
+        Assert.AreEqual(_promotionDto.DateEnd, _promotionRepo.GetFromRepository(_promotionDto.Label).GetDateEnd());
+    }
+    
+    [TestMethod]
+    public void WhenPromotionIsCreatedShouldBeAddedToRepository()
+    {
+        _promotionDto= new PromotionDto("Summer discount", 30, new DateTime(2024, 7, 15), new DateTime(2024, 10, 15));
+        _promotionLogic.CreatePromotion(_promotionDto);
         Assert.AreEqual(_promotionDto.Label, _promotionRepo.GetFromRepository(_promotionDto.Label).GetLabel());
         Assert.AreEqual(_promotionDto.Discount, _promotionRepo.GetFromRepository(_promotionDto.Label).GetDiscount());
         Assert.AreEqual(_promotionDto.DateStart, _promotionRepo.GetFromRepository(_promotionDto.Label).GetDateStart());
