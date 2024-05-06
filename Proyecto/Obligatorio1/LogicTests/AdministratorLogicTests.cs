18367e6222c6c8745b078dfe9b96cd8ed95a75b5
Franco Ramos <francoramos1511@gmail.com>
2024-05-06 00:57:12 -0300

    Changes in all AdministratorLogicTests for clean code

--- Proyecto/Obligatorio1/LogicTests/AdministratorLogicTests.cs
+++ Proyecto/Obligatorio1/LogicTests/AdministratorLogicTests.cs
@@ -12,7 +12,6 @@ public class AdministratorLogicTests
 {
     private PersonRepositories _personRepo;
     private AdministratorLogic _administratorLogic;
-    private AdministratorDto _administratorDto;
     private List<Booking> _bookings;
     private Promotion _promotion;
     private PromotionDto _promotionDto;
@@ -32,13 +31,9 @@ public class AdministratorLogicTests
         _bookings = new List<Booking>();
         _promotions = new List<Promotion>();
         _promotionsDto = new List<PromotionDto>();
-        _administratorDto = new AdministratorDto("Franco", "Ramos", "francoramos1511@gmail.com", "PassWord921#");
-    }
-    
-    [TestMethod]
-    public void WhenAdministratorApprovesABookingDtoShouldChangeItToTrue()
-    {
-        _booking = new Booking(false, new DateTime(2023, 7, 5), new DateTime(2026, 8, 15), new StorageUnit("12", AreaType.A, SizeType.Small, true, new List<Promotion>()), "");
+        _promotion = new Promotion("Winter Discount", 25, new DateTime(2023,7,5), new DateTime(2026,8,15));
+        _promotions.Add(_promotion);
+        _booking = new Booking(false, new DateTime(2023, 7, 5), new DateTime(2026, 8, 15), new StorageUnit("12", AreaType.A, SizeType.Small, true, _promotions), "");
         _bookings.Add(_booking);
         _user = new User("John", "Doe", "johndoe@gmail.com", "PassWord921#", _bookings);
         _personRepo.AddToRepository(_user);
@@ -46,20 +41,17 @@ public class AdministratorLogicTests
         _promotionsDto.Add(_promotionDto);
         _bookingDto = new BookingDto(true, new DateTime(2023, 7, 5), new DateTime(2026, 8, 15), new StorageUnitDto("12", AreaType.A, SizeType.Small, true, _promotionsDto), "");
         _userDto = new UserDto("John", "Doe", "johndoe@gmail.com", "PassWord921#", new List<BookingDto>());
+    }
+    
+    [TestMethod]
+    public void WhenAdministratorApprovesABookingDtoShouldChangeItToTrue()
+    {
         _administratorLogic.ApproveBooking(_userDto, _bookingDto);
     }
 
     [TestMethod]
     public void WhenAdministratorRejectsABookingDtoShouldWriteARejectionMessage()
     {
-        _booking = new Booking(false, new DateTime(2023, 7, 5), new DateTime(2026, 8, 15), new StorageUnit("12", AreaType.A, SizeType.Small, true, new List<Promotion>()), "");
-        _bookings.Add(_booking);
-        _user = new User("John", "Doe", "johndoe@gmail.com", "PassWord921#", _bookings);
-        _personRepo.AddToRepository(_user);
-        _promotionDto = new PromotionDto("Winter Discount", 25, new DateTime(2023, 7, 5), new DateTime(2026, 8, 15));
-        _promotionsDto.Add(_promotionDto);
-        _bookingDto = new BookingDto(true, new DateTime(2023, 7, 5), new DateTime(2026, 8, 15), new StorageUnitDto("12", AreaType.A, SizeType.Small, true, _promotionsDto), "");
-        _userDto = new UserDto("John", "Doe", "johndoe@gmail.com", "PassWord921#", new List<BookingDto>());
         string rejectionMessage = "The booking has been rejected";
         _administratorLogic.SetRejectionMessage(_userDto, _bookingDto, rejectionMessage);
     }
@@ -68,27 +60,12 @@ public class AdministratorLogicTests
     [ExpectedException(typeof(LogicExceptions))]
     public void WhenAdministratorRejectsABookingDtoWithEmptyMessageShouldThrowException()
     {
-        _booking = new Booking(false, new DateTime(2023, 7, 5), new DateTime(2026, 8, 15), new StorageUnit("12", AreaType.A, SizeType.Small, true, new List<Promotion>()), "");
-        _bookings.Add(_booking);
-        _user = new User("John", "Doe", "johndoe@gmail.com", "PassWord921#", _bookings);
-        _personRepo.AddToRepository(_user);
-        _promotionDto = new PromotionDto("Winter Discount", 25, new DateTime(2023, 7, 5), new DateTime(2026, 8, 15));
-        _promotionsDto.Add(_promotionDto);
-        _bookingDto = new BookingDto(true, new DateTime(2023, 7, 5), new DateTime(2026, 8, 15), new StorageUnitDto("12", AreaType.A, SizeType.Small, true, _promotionsDto), "");
-        _userDto = new UserDto("John", "Doe", "johndoe@gmail.com", "PassWord921#", new List<BookingDto>());
         _administratorLogic.SetRejectionMessage(_userDto, _bookingDto, "");
     }
 
     [TestMethod]
     public void WhenAdministratorIsTryingToApproveOrRejectBookingsShouldGetAListOfUsersDto()
     {
-        _promotion = new Promotion("Winter Discount", 25, new DateTime(2023,7,5), new DateTime(2026,8,15));
-        _promotions.Add(_promotion);
-        _storageUnit = new StorageUnit("12", AreaType.A, SizeType.Small, true, _promotions);
-        Booking booking = new Booking(false, new DateTime(2023, 7, 5), new DateTime(2026, 8, 15), _storageUnit, "");
-        _bookings.Add(booking);
-        User user = new User("John", "Doe", "johndoe@gmail.com", "PassWord921#", _bookings);
-        _personRepo.AddToRepository(user);
         List<UserDto> users = _administratorLogic.GetUsersDto();
         Assert.IsTrue(users.Count > 0);
     }
