1af4efa47db212581a9bab79799c912cd93e82b0
Federico Gutierrez <fedeguti521@gmail.com>
2024-05-23 20:33:24 -0300

    [REFACTOR] WhenAddingNewPersonShouldAddItToRepository (property changes)

--- Proyecto/Obligatorio1/RepositoriesTests/PersonRepositoriesTests.cs
+++ Proyecto/Obligatorio1/RepositoriesTests/PersonRepositoriesTests.cs
@@ -20,8 +20,8 @@ public class PersonRepositoryTest
     public void WhenAddingNewPersonShouldAddItToRepository()
     {
         _personRepo.AddToRepository(_person);
-        Person personInRepo = _personRepo.GetFromRepository(_person.GetEmail());
-        Assert.AreEqual(_person.GetEmail(), personInRepo.GetEmail());
+        Person personInRepo = _personRepo.GetFromRepository(_person.Email);
+        Assert.AreEqual(_person.Email, personInRepo.Email);
     }
     
     [TestMethod]
@@ -38,7 +38,7 @@ public class PersonRepositoryTest
     {
         _personRepo.AddToRepository(_person);
         _personRepo.RemoveFromRepository(_person);
-        Assert.IsFalse(_personRepo.ExistsInRepository(_person.GetEmail()));
+        Assert.IsFalse(_personRepo.ExistsInRepository(_person.Email));
     }
 
     [TestMethod]
