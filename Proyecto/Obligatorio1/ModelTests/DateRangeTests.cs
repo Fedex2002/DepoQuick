3457c66dddd65c7326c127724da99f0ba19e16b4
Franco Ramos <francoramos1511@gmail.com>
2024-05-24 11:47:20 -0300

    [RED] CreatingEmptyDateRangeShouldReturnEmpty

---
+++ Proyecto/Obligatorio1/ModelTests/DateRangeTests.cs
@@ -0,0 +1,19 @@
+namespace ModelTests;
+
+[TestClass]
+public class DateRangeTests
+{
+    private DateRange _dateRange;
+    
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _dateRange = new DateRange();
+    }
+    
+    [TestMethod]
+    public void CreatingEmptyDateRangeShouldReturnEmpty()
+    {
+        Assert.IsNotNull(_dateRange);
+    }
+}
