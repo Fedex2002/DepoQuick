6f4ca1419fb69526cd9f27e19407b5fc2848113d
Franco Ramos <francoramos1511@gmail.com>
2024-05-25 16:18:27 -0300

    [REFACTOR] WhenAdministratorApprovesABookingDtoShouldChangeItToTrueAndToStatusCaptured

--- Proyecto/Obligatorio1/LogicTests/AdministratorLogicTests.cs
+++ Proyecto/Obligatorio1/LogicTests/AdministratorLogicTests.cs
@@ -59,6 +59,9 @@ public class AdministratorLogicTests
     [TestMethod]
     public void WhenAdministratorApprovesABookingDtoShouldChangeItToTrueAndToStatusCaptured()
     {
+        _bookingDto = new BookingDto(false, new DateTime(2023, 7, 5), new DateTime(2026, 8, 15),
+            new StorageUnitDto("12", AreaType.A, SizeType.Small, true, _promotionsDto, _availableDatesDto), "",
+            "Reservado", true);
         _administratorLogic.ApproveBooking(_userDto, _bookingDto);
     }
 
