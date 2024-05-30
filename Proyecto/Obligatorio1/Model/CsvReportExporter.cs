908de5ac9c42889eb91e20d13adccdecd5ea0e9e
Federico Gutierrez <fedeguti521@gmail.com>
2024-05-30 16:03:35 -0300

    Merge branch 'feature/CsvReportExporter' into feature/Logic

---
+++ Proyecto/Obligatorio1/Model/CsvReportExporter.cs
@@ -0,0 +1,24 @@
+namespace Model;
+
+public class CsvReportExporter
+{
+
+    public CsvReportExporter()
+    {
+    }
+
+    public void Export(string path, List<Booking> bookings)
+    {
+        File.WriteAllText(path, GetData(bookings));
+    }
+    
+    public string GetData(List<Booking> bookings)
+    {
+        string data = "DEPOSITO,RESERVA,PAGO\r\n";
+        foreach (var booking in bookings)
+        {
+            data += booking.StorageUnit.Id +"," +booking.StorageUnit.Area+ "," +booking.StorageUnit.Climatization+ "," +booking.StorageUnit.AvailableDates.Count + "," +booking.StorageUnit.Size+ "," +booking.StorageUnit.Promotions.Count + "," + booking.Approved + "," + booking.DateStart + "," + booking.DateEnd + "," + booking.RejectedMessage + "," + booking.Status + "," + booking.Payment + "\r\n";
+        }
+        return data;
+    }
+}
