4fad6db076ea8a7260e508503e305feadc543bb9
Franco Ramos <francoramos1511@gmail.com>
2024-06-01 16:38:31 -0300

    [RED] WhenAddingANewPromotion_ShouldAddTheNewPromotionInPromotionsTable

---
+++ Proyecto/Obligatorio1/DataAccessTests/PromotionRepositoryTests.cs
@@ -0,0 +1,38 @@
+using DataAccess.Context;
+using Model;
+
+namespace DataAccessTests;
+
+[TestClass]
+public class PromotionRepositoryTests
+{
+    private ApplicationDbContext _context;
+    private readonly IApplicationDbContextFactory _contextFactory = new InMemoryAppContextFactory();
+    private Promotion _myPromotion;
+    
+    [TestInitialize]
+    public void SetUp()
+    {
+        _context = _contextFactory.CreateDbContext();
+        _repository = new PromotionsRepository(_context);
+        DateTime dateStart = new DateTime(2024,7,15);
+        DateTime dateEnd = new DateTime(2024,10,15);
+        _myPromotion = new Promotion("Descuento Invierno", 25, dateStart, dateEnd);
+    }
+    
+    [TestCleanup]
+    public void CleanUp()
+    {
+        _context.Database.EnsureDeleted();
+    }
+    
+    [TestMethod]
+    public void WhenAddingANewPromotion_ShouldAddTheNewPromotionInPromotionsTable()
+    {
+        _repository.AddPromotion(_myPromotion);
+
+        var promotionInDb = _context.Promotions.First();
+        
+        Assert.AreEqual(_myPromotion, promotionInDb);
+    }
+}
