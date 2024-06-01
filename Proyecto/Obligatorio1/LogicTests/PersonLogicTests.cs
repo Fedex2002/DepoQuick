a3c881de16deb8a8397a91cb00fa221819069ae6
Federico Gutierrez <fedeguti521@gmail.com>
2024-06-01 10:58:16 -0300

    [REFACTOR] WhenPersonIsTryingToLoginAndIsUserShouldReturnPersonWithoutAdminPrivileges

--- Proyecto/Obligatorio1/LogicTests/PersonLogicTests.cs
+++ Proyecto/Obligatorio1/LogicTests/PersonLogicTests.cs
@@ -94,7 +94,6 @@ public class PersonLogicTests
     public void WhenPersonIsTryingToLoginAndIsUserShouldReturnPersonWithoutAdminPrivileges()
     {
         _person.IsAdmin = false;
-        _personRepo.AddToRepository(_person);
         PersonDto loggedInPersonDto = _personLogic.Login(_person.Email,_person.Password);
         
         Assert.AreEqual(_person.Name, loggedInPersonDto.Name);
