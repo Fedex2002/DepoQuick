858f33abe1b191a1efe2a210c1e94f203ed6a618
Federico Gutierrez <fedeguti521@gmail.com>
2024-06-02 18:35:44 -0300

    Changes in Add method for IPersonController

--- Proyecto/Obligatorio1/Logic/Interfaces/IPersonController.cs
+++ Proyecto/Obligatorio1/Logic/Interfaces/IPersonController.cs
@@ -6,6 +6,6 @@ namespace Logic.Interfaces;
 public interface IPersonController
 {
     public Person CreatePerson(PersonDto personDto);
-    public void AddUser(PersonDto personDto);
+    public void AddPerson(PersonDto personDto);
     public List<PersonDto> GetPersonsDto();    
 }
