257787f60e5986732552f31961b46f59fbb9a0c3
Franco Ramos <francoramos1511@gmail.com>
2024-06-02 16:43:48 -0300

    [REFACTOR] WhenPersonAlreadyExists_ShouldThrowRepositoryException

--- Proyecto/Obligatorio1/DataAccess/Repository/PersonsRepository.cs
+++ Proyecto/Obligatorio1/DataAccess/Repository/PersonsRepository.cs
@@ -17,13 +17,18 @@ public class PersonsRepository
     {
         if (PersonAlreadyExists(person))
         {
-            throw new RepositoryExceptions("The person already exists");
+            PersonAlreadyExistsSoThrowException();
         }
         _database.Persons.Add(person);
 
         _database.SaveChanges();
     }
-    
+
+    private static void PersonAlreadyExistsSoThrowException()
+    {
+        throw new RepositoryExceptions("The person already exists");
+    }
+
     public bool PersonAlreadyExists(Person newPerson)
     {
         return _database.Persons.Any(person => person.Email == newPerson.Email);
