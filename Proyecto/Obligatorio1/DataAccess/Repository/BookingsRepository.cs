9c4b3d5887104e33d13ee4a728ef1c92e0ad129b
Franco Ramos <francoramos1511@gmail.com>
2024-06-02 18:28:55 -0300

    [REFACTOR] WhenBookingAlreadyExists_ShouldThrowRepositoryException

--- Proyecto/Obligatorio1/DataAccess/Repository/BookingsRepository.cs
+++ Proyecto/Obligatorio1/DataAccess/Repository/BookingsRepository.cs
@@ -1,5 +1,6 @@
 using DataAccess.Context;
 using Model;
+using Model.Exceptions;
 
 namespace DataAccess.Repository;
 
@@ -14,11 +15,21 @@ public class BookingsRepository
     
     public void AddBooking(Booking booking)
     {
+        if (BookingAlreadyExists(booking))
+        {
+            BookingAlreadyExistsSoThrowException();
+        }
+        
         _database.Bookings.Add(booking);
 
         _database.SaveChanges();
     }
-    
+
+    private static void BookingAlreadyExistsSoThrowException()
+    {
+        throw new RepositoryExceptions("Booking already exists");
+    }
+
     public bool BookingAlreadyExists(Booking booking)
     {
         return _database.Bookings.Any(b => b == booking);
