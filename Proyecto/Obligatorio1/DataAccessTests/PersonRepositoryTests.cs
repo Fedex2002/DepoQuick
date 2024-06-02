756456882078dafc01688fca81c44fc06ea5d3cb
Franco Ramos <francoramos1511@gmail.com>
2024-06-02 16:31:08 -0300

    [RED] WhenAddingANewPerson_ShouldAddTheNewPersonInPersonsTable

---
+++ Proyecto/Obligatorio1/DataAccessTests/PersonRepositoryTests.cs
@@ -0,0 +1,36 @@
+using DataAccess.Context;
+using Model;
+
+namespace DataAccessTests;
+
+[TestClass]
+public class PersonRepositoryTests
+{
+    private PersonsRepository _repository;
+    private ApplicationDbContext _context;
+    private readonly IApplicationDbContextFactory _contextFactory = new InMemoryAppContextFactory();
+    private Person _person;
+
+    [TestInitialize]
+    public void SetUp()
+    {
+        _context = _contextFactory.CreateDbContext();
+        _person = new Person("John", "Doe", "johndoe@gmail.com", "PassWord921#", false);
+    }
+
+    [TestCleanup]
+    public void CleanUp()
+    {
+        _context.Database.EnsureDeleted();
+    }
+
+    [TestMethod]
+    public void WhenAddingANewPerson_ShouldAddTheNewPersonInPersonsTable()
+    {
+        _repository.AddPerson(_person);
+
+        var personInDb = _context.Persons.First();
+
+        Assert.AreEqual(_person, personInDb);
+    }
+}
