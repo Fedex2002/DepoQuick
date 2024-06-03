0c52ebdc93d71aa719b02a98739c569a157f67e9
Franco Ramos <francoramos1511@gmail.com>
2024-06-03 13:42:16 -0300

    [REFACTOR] WhenStorageUnitAlreadyExists_ShouldThrowRepositoryException

--- Proyecto/Obligatorio1/DataAccess/Repository/StorageUnitsRepository.cs
+++ Proyecto/Obligatorio1/DataAccess/Repository/StorageUnitsRepository.cs
@@ -17,14 +17,19 @@ public class StorageUnitsRepository
     {
         if (StorageUnitAlreadyExists(storageUnit))
         {
-            throw new RepositoryExceptions("Storage unit already exists");
+            StorageUnitAlreadyExistsSoThrowException();
         }
         
         _database.StorageUnits.Add(storageUnit);
         
         _database.SaveChanges();
     }
-    
+
+    private static void StorageUnitAlreadyExistsSoThrowException()
+    {
+        throw new RepositoryExceptions("Storage unit already exists");
+    }
+
     public bool StorageUnitAlreadyExists(StorageUnit storageUnit)
     {
         return _database.StorageUnits.Any(s => s == storageUnit);
