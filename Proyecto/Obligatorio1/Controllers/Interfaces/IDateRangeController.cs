4fd00f39aaae680da600bed9dec28e81c08961d8
Franco Ramos <francoramos1511@gmail.com>
2024-06-05 18:58:34 -0300

    Minimal changes in IStorageUnitController and IDateRangeDtoController

--- Proyecto/Obligatorio1/Controllers/Interfaces/IDateRangeController.cs
+++ Proyecto/Obligatorio1/Controllers/Interfaces/IDateRangeController.cs
@@ -6,4 +6,5 @@ public interface IDateRangeController
 {
     public void CheckIfDateStartAndDateEndAreIncludedInDateRange(DateTime dateStart, DateTime dateEnd, DateRangeDto dateRangeDto);
     public void ModifyOrRemoveDateRange(StorageUnitDto storageUnitDto, DateRangeDto dateRangeDto);
+    public void EliminateDateRangeFromStorageUnit(string id, DateRangeDto dateRangeDto);
 }
