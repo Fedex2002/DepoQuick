f6361d60f9e204d45c3efab874b47e0a08de73f1
Franco Ramos <francoramos1511@gmail.com>
2024-06-05 19:59:43 -0300

    Interface IPersonAuthentication deleted because is unnecessary, methods are added to IPersonController

--- Proyecto/Obligatorio1/Controllers/Interfaces/IPersonController.cs
+++ Proyecto/Obligatorio1/Controllers/Interfaces/IPersonController.cs
@@ -5,9 +5,10 @@ namespace Logic.Interfaces;
 
 public interface IPersonController
 {
-    public Person CreatePerson(PersonDto personDto);
-    public void AddPerson(Person person);
-    public List<PersonDto> GetPersonsDto();    
-    
+    public bool CheckIfEmailIsRegistered(string email);
+    public bool CheckIfPasswordIsCorrect(string password, string verifyPassword);
+    public PersonDto Login(string email, string password);
+    public void SignUp(PersonDto personDto);
+    public PersonDto GetPersonDtoFromEmail(string personEmail);
     public void Logout();
 }
