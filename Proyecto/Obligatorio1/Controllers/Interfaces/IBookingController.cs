bf0d22b7470a2292822555b6539b5f62a2773a9f
Franco Ramos <francoramos1511@gmail.com>
2024-06-05 19:12:29 -0300

    IBookingController modified and method ChangeToStorageUnit made private, test removed

--- Proyecto/Obligatorio1/Controllers/Interfaces/IBookingController.cs
+++ Proyecto/Obligatorio1/Controllers/Interfaces/IBookingController.cs
@@ -6,6 +6,7 @@ namespace Logic.Interfaces;
 public interface IBookingController
 {
     public void CreateBooking(string userEmail, BookingDto bookingDto);
+    public bool CheckIfBookingIsApproved(BookingDto bookingDto);
     public void ApproveBooking(PersonDto personDto, BookingDto bookingDto);
     public void SetRejectionMessage(PersonDto personDto, BookingDto bookingDto, string rejectionMessage);
     public void AddUserToBooking(BookingDto bookingDto, PersonDto personDto);
