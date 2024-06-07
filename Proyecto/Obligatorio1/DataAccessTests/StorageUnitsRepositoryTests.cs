8f666a7a05c06b25a1a1ddba2ba8d72603ae5f99
Federico Gutierrez <fedeguti521@gmail.com>
2024-06-07 15:56:15 -0300

    [GREEN] WhenAddingAValidDateRangeToStorageUnit_ShouldAddTheDateRangeToTheStorageUnit

--- Proyecto/Obligatorio1/DataAccessTests/StorageUnitsRepositoryTests.cs
+++ Proyecto/Obligatorio1/DataAccessTests/StorageUnitsRepositoryTests.cs
@@ -91,11 +91,10 @@ public class StorageUnitsRepositoryTests
     
     public void WhenAddingAValidDateRangeToStorageUnit_ShouldAddTheDateRangeToTheStorageUnit()
     {
+        _repository.AddStorageUnit(_storageUnit);
         DateRange dateRange = new DateRange(DateTime.Now, DateTime.Now.AddDays(1));
-        _storageUnit.AvailableDates.Add(dateRange);
-        _repository.AddAvailableDateToStorageUnit(_storageUnit, dateRange);
+        _repository.AddAvailableDateToStorageUnit(_storageUnit.Id, dateRange);
         StorageUnit storageUnitInDb = _repository.GetStorageUnitFromId(_storageUnit.Id);
-
         Assert.AreEqual(1, storageUnitInDb.AvailableDates.Count);
     }
 }
