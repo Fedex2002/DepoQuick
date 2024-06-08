841229907c406362504b12117b6b39d9df8bea62
Franco Ramos <francoramos1511@gmail.com>
2024-06-08 19:05:08 -0300

    [REFACTOR] WhenUserPaysABookingShouldSetItToTrue

--- Proyecto/Obligatorio1/ControllersTests/BookingControllerTests.cs
+++ Proyecto/Obligatorio1/ControllersTests/BookingControllerTests.cs
@@ -132,6 +132,7 @@ public class BookingControllerTests
     [TestMethod]
     public void WhenUserPaysABookingShouldSetItToTrue()
     {
+        _storageUnitsRepository.AddStorageUnit(_storageUnit);
         _bookingController.CreateBooking(_userDto.Email, _mybookingDto);
         _bookingController.PayBooking(_userDto.Email, _mybookingDto);
     }
