4481e51c1819b258de021aea07489eb6ce6158fa
Federico Gutierrez <fedeguti521@gmail.com>
2024-06-08 01:08:10 -0300

    conflicts resolved

--- Proyecto/Obligatorio1/DataAccessTests/StorageUnitsRepositoryTests.cs
+++ Proyecto/Obligatorio1/DataAccessTests/StorageUnitsRepositoryTests.cs
@@ -96,15 +96,13 @@ public class StorageUnitsRepositoryTests
         StorageUnit storageUnitInDb = _repository.GetStorageUnitFromId(_storageUnit.Id);
         Assert.AreEqual(1, storageUnitInDb.AvailableDates.Count);
     }
-
+    
     [TestMethod]
-    public void WhenDeletingAValidDateRangeFromStorageUnit_ShouldDeleteIt()
+    
+    public void WhenDeletingAValidDateRangeFromStorageUnit_ShouldDeleteTheDateRangeFromTheStorageUnit()
     {
         _repository.AddStorageUnit(_storageUnit);
         DateRange dateRange = new DateRange(DateTime.Now, DateTime.Now.AddDays(1));
         _repository.AddAvailableDateToStorageUnit(_storageUnit.Id, dateRange);
-        _repository.DeleteAvailableDateFromStorageUnit(_storageUnit.Id, dateRange);
-        StorageUnit storageUnitInDb = _repository.GetStorageUnitFromId(_storageUnit.Id);
-        Assert.AreEqual(0, storageUnitInDb.AvailableDates.Count);
     }
 }
