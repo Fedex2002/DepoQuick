8c9371caf2dd2970f7cea2586650fc6d470301f9
Franco Ramos <francoramos1511@gmail.com>
2024-06-08 19:07:52 -0300

    [REFACTOR] WhenAdministratorRejectsABookingDtoShouldWriteARejectionMessage

--- Proyecto/Obligatorio1/ControllersTests/BookingControllerTests.cs
+++ Proyecto/Obligatorio1/ControllersTests/BookingControllerTests.cs
@@ -154,8 +154,9 @@ public class BookingControllerTests
     [TestMethod]
     public void WhenAdministratorRejectsABookingDtoShouldWriteARejectionMessage()
     {
+        _storageUnitsRepository.AddStorageUnit(_storageUnit);
         _mybookingDto = new BookingDto(false, new DateTime(2023, 7, 5), new DateTime(2026, 8, 15),
-            new StorageUnitDto("12", _areaTypeDto, _sizeTypeDto, true, _promotionsDto, _availableDatesDto), "",
+            _storageUnitDto, "",
             "Reservado", true,_person.Email);
         string rejectionMessage = "The booking has been rejected";
         _bookingController.CreateBooking(_userDto.Email, _mybookingDto);
