a566219ac0cba25bbbae53119c69f09c18b5c14a
Franco Ramos <francoramos1511@gmail.com>
2024-06-08 17:01:43 -0300

    [REFACTOR] WhenUserPaysABookingShouldSetItToTrue

--- Proyecto/Obligatorio1/Controllers/BookingController.cs
+++ Proyecto/Obligatorio1/Controllers/BookingController.cs
@@ -63,7 +63,7 @@ public class BookingController : IBookingController
         {
             IfBookingPaymentIsAlreadyTrueThrowException(bookingToPay);
             bookingToPay.Payment = true;
-            bookingDto.Payment = true;
+            _bookingRepositories.UpdateBooking(bookingToPay);
         }
     }
     
